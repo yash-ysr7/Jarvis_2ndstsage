a5d27588158f35b01be6b5f0de4cc5b002881bbe
Yashveer Singh <101798712+Yashveer-Coder@users.noreply.github.com>
2022-11-17 17:14:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,164 @@
+import pyttsx3
+import speech_recognition 
+import datetime
+import requests
+import os
+import pyautogui
+import speedtest
+from bs4 import BeautifulSoup
+
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[0].id)
+rate = engine.setProperty("rate",170)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening.....")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+
+    try:
+        print("Understanding..")
+        query  = r.recognize_google(audio,language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again")
+        return "None"
+    return query
+
+    
+def alarm(query):
+    timehere = open("Alarmtext.txt","a")
+    timehere.write(query)
+    timehere.close()
+    os.startfile("alarm.py")
+    
+if __name__ == "__main__":
+    while True:
+        query = takeCommand().lower()
+        if "daddy's home" or "wake up" in query:
+            from namaste import namaste
+            namaste()
+
+            while True:
+                query = takeCommand().lower()
+                if "go to sleep" in query:
+                    speak("Ok sir , You can me call anytime")
+                    break 
+                elif "hello" in query:
+                    speak("Hello sir, how are you ?")
+                elif "i am fine" in query:
+                    speak("that's great, sir")
+                elif "how are you" in query:
+                    speak("Perfect, sir")
+                elif "thank you" in query:
+                    speak("you are welcome, sir")     
+
+                elif "open" in query:
+                    from Dictapp import openappweb
+                    openappweb(query)
+                elif "close" in query:
+                    from Dictapp import closeappweb
+                    closeappweb(query)             
+
+                elif "google" in query:
+                    from search import searchGoogle
+                    searchGoogle(query)
+                elif "youtube" in query:
+                    from search import searchYoutube
+                    searchYoutube(query)
+                elif "wikipedia" in query:
+                    from search import searchWikipedia
+                    searchWikipedia(query)            
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")
+
+                elif "temperature" in query:
+                    search = "temperature in delhi"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+                elif "weather" in query:
+                    search = "temperature in delhi"
+                    url = f"https://www.google.com/search?q={search}"
+                    r  = requests.get(url)
+                    data = BeautifulSoup(r.text,"html.parser")
+                    temp = data.find("div", class_ = "BNeawe").text
+                    speak(f"current{search} is {temp}")
+
+                elif "final sleep" in query:
+                    speak("Going to sleep,sir")
+                    exit()    
+
+                elif "the time" in query:
+                    strTime = datetime.datetime.now().strftime("%H:%M")    
+                    speak(f"Sir, the time is {strTime}")  
+
+                elif "set an alarm" in query:
+                    print("input time example:- 10 and 10 and 10")
+                    speak("Set the time")
+                    a = input("Please tell the time :- ")
+                    alarm(a)
+                    speak("Done,sir")    
+
+                elif "pause" in query:
+                    pyautogui.press("k")
+                    speak("video paused")
+                elif "play" in query:
+                    pyautogui.press("k")
+                    speak("video played")
+                elif "silence" in query:
+                    pyautogui.press("m")
+                    speak("video silenced")
+
+                elif "volume up" in query:
+                    from keyboard import volumeup
+                    speak("Turning volume up,sir")
+                    volumeup()
+                elif "volume down" in query:
+                    from keyboard import volumedown
+                    speak("Turning volume down, sir")
+                    volumedown()             
+
+                elif "calculate" in query:
+                   from Calculatenumbers import WolfRamAlpha
+                   from Calculatenumbers import Calc
+                   query = query.replace("calculate","")
+                   query = query.replace("jarvis","")
+                   Calc(query)   
+
+                elif "open" in query:   
+                    query = query.replace("open","")
+                    query = query.replace("jarvis","")
+                    pyautogui.press("super")
+                    pyautogui.typewrite(query)
+                    pyautogui.sleep(2)
+                    pyautogui.press("enter")  
+
+                elif "internet speed" in query:
+                    wifi  = speedtest.Speedtest()
+                    upload_net = wifi.upload()/1048576         #Megabyte = 1024*1024 Bytes
+                    download_net = wifi.download()/1048576
+                    print("Wifi Upload Speed is", upload_net)
+                    print("Wifi download speed is ",download_net)
+                    speak(f"Wifi download speed is {download_net}")
+                    speak(f"Wifi Upload speed is {upload_net}")                     
+
+                elif "translate" in query:
+                    from Translator import translategl
+                    query = query.replace("jarvis","")
+                    query = query.replace("translate","")
+                    translategl(query)
+                    
+                        
